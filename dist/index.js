b60398318a4e809aaeb847a0c95849032883671d
Tony Tran <toant13@users.noreply.github.com>
2018-12-21 22:54:24 -0500

    QUAD-727 Implemented order book depth (#3)
    * QUAD-727 Added order book depth function

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
-var _okexApi = require("./okex-api");
+var _okexApi = require('./okex-api');
 
 var _okexApi2 = _interopRequireDefault(_okexApi);
 
-var _okexApiWs = require("./okex-api-ws");
+var _okexApiWs = require('./okex-api-ws');
 
 var _okexApiWs2 = _interopRequireDefault(_okexApiWs);
 
